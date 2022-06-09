ccd094b6ec7f7f7bc70b2b7e9744fea25f1d5893
Uriel <christian.castellon18@tectijuana.edu.mx>
2022-06-09 16:59:47 -0700

    creacion creacion first y second fragment first fragment falta acomodar diseño second fragment falta poner el contenido del curso 1 en fragment second

--- app/src/main/java/com/devappmovil20221/proyectofinal/FirstFragment.java
+++ app/src/main/java/com/devappmovil20221/proyectofinal/FirstFragment.java
@@ -8,8 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class FirstFragment extends Fragment {
+    // Declaracion de variables miembro
+    private ImageButton mimagebutton_curso1;
 
+    public FirstFragment(){
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        mimagebutton_curso1 = view.findViewById(R.id.imagebutton_curso1);
+        mimagebutton_curso1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new SecondFragment(), null).addToBackStack(null).commit();
+            }
+        });
+        return view;
+    }
 }
---
+++ app/src/main/java/com/devappmovil20221/proyectofinal/SecondFragment.java
@@ -0,0 +1,13 @@
+package com.devappmovil20221.proyectofinal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment {
+
+}
